8d2f4d4b3be387eb4c84786236ea8ca4b8e92c9d
meharinkoommen <meharinkoommen@gmail.com>
2026-09-22 17:32:13 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,38 @@
+print("Welcome to python")
+#if it is a string you need to mention inside the double quote
+#print statement is used  to display information on the output screen\console
+
+'''
+
+HOW TO RUN :
+top-> click run-> run without debugging
+right click-> open in integrated terminal
+right click -> run python file in  terminal
+
+'''
+print(34)
+print("34")
+print(98.5)
+print("hello")
+
+#python is a case sensitive language
+
+
+'''
+
+---> COMMENTS:
+       It is used for code understanding purpose
+       python doesnot excute the comments
+eg: print("hello")
+# print statement
+run-> Hello
+
+
+--->TYPES OF COMMENTS:
+1. Single line comment:
+            It is defined by # 
+
+2. Multiple line comments:
+            It is defined by triple quotes
+
+'''
